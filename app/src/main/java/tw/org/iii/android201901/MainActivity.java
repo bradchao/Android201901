e2c7476552e81e8d3ad38d31f72b9c2f3d270ca2
brad <brad@brad.tw>
2019-11-22 20:05:24 +0800

    6

--- app/src/main/java/tw/org/iii/android201901/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201901/MainActivity.java
@@ -3,12 +3,21 @@ package tw.org.iii.android201901;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView lottery;
+    private Button click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lottery = findViewById(R.id.lottery);
+        click = findViewById(R.id.click);
+
+
     }
 }
